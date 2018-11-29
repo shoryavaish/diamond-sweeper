ef751fa6c94d68dcedfae8860a491062cc8b5a20
Shorya Vaish <shorya@dentalkart.com>
2018-11-29 13:40:53 +0530

    comments added in logic

--- app/client/app.js
+++ app/client/app.js
@@ -3,22 +3,24 @@ global.startApp = function(container) {
   	init();
 }
 
-var diamondsRowWise = {};
-var diamondsColumnWise = {};
-var count = 0;
-var openDiamonds = 0;
-var sizeOfBox = 0;
-var rowDiff = null, columnDiff = null;
-var rowIndex = null, columnIndex = null;
+var diamondsRowWise = {}; // object that stores all the randomly generated diamonds according to row 
+var diamondsColumnWise = {}; // object that stores all the randomly generated diamonds according to column
+var count = 0; // counter to check all diamond must be placed at diffrent positions
+var openDiamonds = 0; // counter to check number of diamonds found
+var sizeOfBox = 0; // matrix size
+var rowDiff = null; // save the nearest distance of diamond in a row 
+var columnDiff = null; // save the nearest distance of diamond in a column 
+var rowIndex = null; // nearest index of diamond in a row
+var columnIndex = null; // nearest index of diamond in a column
 
-document.addEventListener('click', clickBox);
+document.addEventListener('click', clickBox); // added a listner to get all the click events over boxes
 
 function init(){
-	sizeOfBox = document.getElementsByTagName('tr').length;
+	sizeOfBox = document.getElementsByTagName('tr').length; // get size of box
 
 	while(count!==sizeOfBox){
-		var a = getRandomInt(sizeOfBox);
-		var b = getRandomInt(sizeOfBox);
+		var a = getRandomInt(sizeOfBox); // random row number
+		var b = getRandomInt(sizeOfBox); // random column number
 		if(!diamondsRowWise[a])
 			diamondsRowWise[a] = {};
 		if(!diamondsColumnWise[b])
@@ -31,11 +33,11 @@ function init(){
 	}
 }
 
-function getRandomInt(max) {
+function getRandomInt(max) { // generate rondom value b/w 0-sizeOfBox
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function getRowDiffrenceAndIndex(row, column){
+function getRowDiffrenceAndIndex(row, column){ // this calculates the shortest distance and index of diamond in a row
 	var diamondsInColumn = Object.keys(diamondsRowWise[row]);
 	
 	for(let i = 0; i < diamondsInColumn.length; i++){
@@ -47,7 +49,7 @@ function getRowDiffrenceAndIndex(row, column){
 	}
 }
 
-function getColumnDiffrenceAndIndex(row, column){
+function getColumnDiffrenceAndIndex(row, column){ // this calculates the shortest distance and index of diamond in a column
 	var diamondsInRow = Object.keys(diamondsColumnWise[column]);
 	
 	for(let i = 0; i < diamondsInRow.length; i++){
@@ -59,54 +61,65 @@ function getColumnDiffrenceAndIndex(row, column){
 	}
 }
 
-function rotateArrowRowWise(ev, column){
+function rotateArrowRowWise(ev, column){ // function to rotate an arrow in a row i.e. left and right direction 
 	if(rowIndex < column)
 		ev.target.style.transform = 'rotate(180deg)';
 	else
 		ev.target.style.transform = 'rotate(0deg)';
 }
 
-function rotateArrowColumnWise(ev, row){
+function rotateArrowColumnWise(ev, row){ // function to rotate an arrow in a column i.e. up and down direction 
 	if(columnIndex < row)
 		ev.target.style.transform = 'rotate(270deg)';
 	else
 		ev.target.style.transform = 'rotate(90deg)';
 }
 
-function clickBox(ev){
+function checkDiamond(row, column){ // function to return whether diamond is hidden in that block or not
+	return diamondsRowWise[row] && diamondsRowWise[row][column];
+}
+
+function clickBox(ev){ 
 	if(ev.target.className.includes('cell')){
-		var row = ev.target.parentNode.parentNode.rowIndex;
+		// get row and column where player clicked
+		var row = ev.target.parentNode.parentNode.rowIndex; 
 		var column = ev.target.parentNode.cellIndex;
-		if(checkDiamond(row, column)){
+
+		if(checkDiamond(row, column)){ // check for a diamond in the clicked row and column
 			if(ev.target.classList.contains('unknown')){
 				ev.target.classList.remove('unknown');
 				ev.target.classList.add('diamond');
-				openDiamonds++;
+				openDiamonds++; // increase the counter of diamonds found
 				
-				if(openDiamonds===sizeOfBox){
+				if(openDiamonds===sizeOfBox){ // check if we get all the diamonds
 					document.getElementsByClassName('messages')[0].innerHTML = 'Hurray! you got all the diamonds.';
 				}
 			}
-		}else{
+		}else{ // shows empty box if there is no diamond
 			if(ev.target.classList.contains('unknown'))
 				ev.target.classList.remove('unknown');
 			else{
+				// if clicked on empty box
 
+				// remove previous arrow from the board
 				if(document.getElementsByClassName('arrow')[0])
 					document.getElementsByClassName('arrow')[0].classList.remove('arrow');
 				
+				// initialize all variables to initial value
 				rowDiff = null;
 				columnDiff = null;
 				rowIndex = null;
 				columnIndex = null;
 
-				if(diamondsRowWise[row])
+				if(diamondsRowWise[row]) // here we check diamond in a row
 					getRowDiffrenceAndIndex(row, column);
-				if(diamondsColumnWise[column])
+				if(diamondsColumnWise[column]) // here we check diamond in a column
 					getColumnDiffrenceAndIndex(row, column);
 
 				if(rowDiff != null  || columnDiff != null){
-					ev.target.classList.add('arrow');
+					ev.target.classList.add('arrow'); // add an arrow if diamond(s) is/are found in respective row and column
+
+					// check for the nearest distance and rotate according to it
 
 					if(columnDiff != null && rowDiff != null){
 						if(rowDiff <= columnDiff)
@@ -122,8 +135,4 @@ function clickBox(ev){
 		}
 	}else
 		return;
-}
-
-function checkDiamond(row, column){
-	return diamondsRowWise[row] && diamondsRowWise[row][column];
 }
